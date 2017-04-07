adc43184005431b111c5f28162d970dff93d95fe
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2017-04-07 20:36:14 +0200

    POC: draw contours on camera view using NDK.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,27 @@
 #include <jni.h>
-#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_me_reinaertvandecruys_bachelorthesis_MainActivity_stringFromJNI(
+JNIEXPORT void JNICALL
+Java_me_reinaertvandecruys_bachelorthesis_MainActivity_process(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject thiz,
+        jlong frameAddr,
+        jlong grayAddr) {
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::Scalar color = cv::Scalar(255, 0, 0);
+
+    cv::Mat &frame = *(cv::Mat *) frameAddr;
+    cv::Mat &gray = *(cv::Mat *) grayAddr;
+
+    cv::threshold(gray, gray, 40, 255, cv::THRESH_BINARY);
+
+    findContours(gray, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i = 0; i < contours.size(); i++) {
+        drawContours(frame, contours, i, color, 2, 8, hierarchy, 0);
+    }
 }
--- app/src/main/java/me/reinaertvandecruys/bachelorthesis/MainActivity.java
+++ app/src/main/java/me/reinaertvandecruys/bachelorthesis/MainActivity.java
@@ -1,29 +1,139 @@
 package me.reinaertvandecruys.bachelorthesis;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+public class MainActivity extends Activity
+        implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final int PERMISSION_CAMERA_REQUEST_ID = 0;
+
+    private JavaCameraView mCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    if (mCameraView != null) {
+                        System.loadLibrary("native-lib");
+                        mCameraView.enableView();
+                    }
+                    break;
+                default:
+                    super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+
+        process(frame.getNativeObjAddr(), inputFrame.gray().getNativeObjAddr());
+
+        return frame;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mCameraView.enableFpsMeter();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CAMERA_REQUEST_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setUpCameraView();
+            }
+        }
+
+        initOpenCV();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        initOpenCV();
+    }
+
+    @SuppressLint("InlinedApi")
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            setUpCameraView();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_CAMERA_REQUEST_ID);
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    private void initOpenCV() {
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    private native void process(long frameAddr, long grayAddr);
+
+    private void setUpCameraView() {
+        mCameraView = (JavaCameraView) findViewById(R.id.javaCameraView);
+        mCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mCameraView.setCvCameraViewListener(this);
+    }
 }
