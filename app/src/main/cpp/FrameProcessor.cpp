023f4058e1e297f0804db83fa4282001796832f1
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2017-04-17 23:17:29 +0200

    Implement tag recognition.

--- app/src/main/cpp/FrameProcessor.cpp
+++ app/src/main/cpp/FrameProcessor.cpp
@@ -1,28 +1,103 @@
 #include "FrameProcessor.h"
 
 void FrameProcessor::process(cv::Mat &frame, cv::Mat &gray) {
+    this->frame = &frame;
+
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
+    getContours(gray, contours, hierarchy);
 
-    cv::threshold(gray, gray, THRESHOLD, THRESHOLD_MAX, cv::THRESH_BINARY);
-
-    findContours(gray, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-
-    for (int i = 0; i < contours.size(); i++) {
-        double contourLength = cv::arcLength(contours[i], true);
+    for (int contourIndex = 0; contourIndex < contours.size(); contourIndex++) {
+        const double contourLength = cv::arcLength(contours[contourIndex], true);
 
         if (contourLength < MIN_ARC_LENGTH || contourLength > MAX_ARC_LENGTH) {
             continue;
         }
 
         std::vector<cv::Point> contourApproximation;
-        cv::approxPolyDP(contours[i], contourApproximation,
+        cv::approxPolyDP(contours[contourIndex], contourApproximation,
                          APPROX_CURVE_ACCURACY_FACTOR * contourLength, true);
 
         if (contourApproximation.size() != 4) {
             continue;
         }
 
-        cv::polylines(frame, contourApproximation, true, COLOR);
+        cv::Point2i centroid = getContourCentroid(contourApproximation);
+
+        const cv::Vec4b color = frame.at<cv::Vec4b>(centroid);
+
+        Tag *tag = getTagByColor(color);
+
+        if (tag == nullptr) {
+            continue;
+        }
+
+        cv::polylines(frame, contourApproximation, true, tag->getColor(), 8);
     }
+}
+
+inline void FrameProcessor::getContours(cv::Mat &gray,
+                                        std::vector<std::vector<cv::Point> > &contours,
+                                        std::vector<cv::Vec4i> &hierarchy) {
+    cv::threshold(gray, gray, THRESHOLD, THRESHOLD_MAX, cv::THRESH_BINARY);
+    findContours(gray, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+}
+
+inline cv::Point2i
+FrameProcessor::getContourCentroid(const std::vector<cv::Point> &contour) {
+    cv::Moments moments = cv::moments(contour);
+
+    return cv::Point2i((int) round(moments.m10 / moments.m00),
+                       (int) round(moments.m01 / moments.m00));
+}
+
+FrameProcessor::Tag *FrameProcessor::getTagByColor(const cv::Vec4b &color) const {
+    cv::Vec4b maximizedColor = getMaximizedColor(color);
+
+    for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+        Tag &tag = *tags[tagIndex];
+        const cv::Vec4b &tagColor = tag.getColor();
+        bool isMatch = true;
+
+        for (int primaryColorIndex = 0;
+             primaryColorIndex < NUM_PRIMARY_COLORS; primaryColorIndex++) {
+            if (abs(tagColor[primaryColorIndex] - maximizedColor[primaryColorIndex])
+                > COLOR_THRESHOLD) {
+                isMatch = false;
+                break;
+            }
+        }
+
+        if (isMatch) {
+            return tags[tagIndex];
+        }
+    }
+
+    return nullptr;
+}
+
+cv::Vec4b FrameProcessor::getMaximizedColor(const cv::Vec4b &color) {
+    cv::Vec4b maximizedColor;
+    int highestPrimaryColorValue = 0;
+
+    for (int primaryColorIndex = 0; primaryColorIndex < NUM_PRIMARY_COLORS; primaryColorIndex++) {
+        highestPrimaryColorValue = highestPrimaryColorValue > color[primaryColorIndex] ?
+                                   highestPrimaryColorValue : color[primaryColorIndex];
+    }
+
+    highestPrimaryColorValue = std::max(highestPrimaryColorValue, 1);
+
+    double scaleFactor = highestPrimaryColorValue >= MIN_MAX_PRIMARY_COLOR_VALUE ?
+                         MAX_PRIMARY_COLOR_VALUE / highestPrimaryColorValue : 0;
+
+    for (int primaryColorIndex = 0; primaryColorIndex < NUM_PRIMARY_COLORS; primaryColorIndex++) {
+        maximizedColor[primaryColorIndex] =
+                (unsigned char) (color[primaryColorIndex] * scaleFactor);
+    }
+
+    return maximizedColor;
+}
+
+const cv::Vec4b &FrameProcessor::Tag::getColor() const {
+    return color;
 }
--- app/src/main/cpp/FrameProcessor.h
+++ app/src/main/cpp/FrameProcessor.h
@@ -11,15 +11,47 @@ public:
     void process(cv::Mat &frame, cv::Mat &gray);
 
 private:
-    const int THRESHOLD = 40;
-    const int THRESHOLD_MAX = 255;
+    class Tag {
+    public:
+        Tag(const cv::Vec4b color) : color(color) {};
 
-    const cv::Scalar COLOR = cv::Scalar(255, 0, 0);
+        const cv::Vec4b &getColor() const;
 
-    const int MIN_ARC_LENGTH = 400;
-    const int MAX_ARC_LENGTH = 2000;
+    private:
+        const cv::Vec4b color;
+    };
 
-    const double APPROX_CURVE_ACCURACY_FACTOR = 0.1;
+    static const int THRESHOLD = 40;
+    static const int THRESHOLD_MAX = 255;
+
+    static const int NUM_PRIMARY_COLORS = 3;
+    static const int MAX_PRIMARY_COLOR_VALUE = 255;
+
+    static const unsigned char COLOR_THRESHOLD = 150;
+    static const unsigned char MIN_MAX_PRIMARY_COLOR_VALUE = 32;
+
+    static const int MIN_ARC_LENGTH = 400;
+    static const int MAX_ARC_LENGTH = 2000;
+
+    static constexpr double APPROX_CURVE_ACCURACY_FACTOR = 0.1;
+
+    cv::Mat *frame;
+
+    Tag tagBlue = Tag(cv::Vec4b(255, 0, 0));
+    Tag tagGreen = Tag(cv::Vec4b(0, 255, 0));
+    Tag tagRed = Tag(cv::Vec4b(0, 0, 255));
+
+    const std::vector<Tag *> tags = {&tagBlue, &tagGreen, &tagRed};
+
+    static inline void getContours(cv::Mat &gray,
+                                   std::vector<std::vector<cv::Point> > &contours,
+                                   std::vector<cv::Vec4i> &hierarchy);
+
+    static inline cv::Point2i getContourCentroid(const std::vector<cv::Point> &contour);
+
+    inline Tag *getTagByColor(const cv::Vec4b &color) const;
+
+    static inline cv::Vec4b getMaximizedColor(const cv::Vec4b &color);
 };
 
 
