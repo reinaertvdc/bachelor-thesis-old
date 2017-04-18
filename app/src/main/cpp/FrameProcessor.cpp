074caa2806383bc80cdbef8ec4fe531e686ff218
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2017-04-18 16:54:09 +0200

    Implement drawing panel contour (but only when looking straight at the panel).

--- app/src/main/cpp/FrameProcessor.cpp
+++ app/src/main/cpp/FrameProcessor.cpp
@@ -3,7 +3,11 @@
 void FrameProcessor::process(cv::Mat &frame, cv::Mat &gray) {
     this->frame = &frame;
 
-    std::vector<std::vector<cv::Point> > contours;
+    for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+        tags[tagIndex]->notifyNewFrame();
+    }
+
+    std::vector<std::vector<cv::Point2i> > contours;
     std::vector<cv::Vec4i> hierarchy;
     getContours(gray, contours, hierarchy);
 
@@ -14,11 +18,11 @@ void FrameProcessor::process(cv::Mat &frame, cv::Mat &gray) {
             continue;
         }
 
-        std::vector<cv::Point> contourApproximation;
+        std::vector<cv::Point2i> contourApproximation;
         cv::approxPolyDP(contours[contourIndex], contourApproximation,
                          APPROX_CURVE_ACCURACY_FACTOR * contourLength, true);
 
-        if (contourApproximation.size() != 4) {
+        if (contourApproximation.size() != NUM_TAG_POINTS) {
             continue;
         }
 
@@ -32,19 +36,59 @@ void FrameProcessor::process(cv::Mat &frame, cv::Mat &gray) {
             continue;
         }
 
-        cv::polylines(frame, contourApproximation, true, tag->getColor(), 8);
+        tag->setPoints(contourApproximation);
+    }
+
+    for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+        tags[tagIndex]->draw();
+    }
+
+    bool tagsMissing = false;
+    for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+        const std::vector<cv::Point2i> *tagContour = tags[tagIndex]->getPoints();
+
+        if (tagContour == nullptr) {
+            tagsMissing = true;
+            break;
+        }
+
+        tagsContour[tagIndex] = getContourCentroid(*tagContour);
+
+        tags[tagIndex]->draw();
+    }
+
+    if (tagsMissing) {
+        return;
+    }
+
+    tagsCenter = (tagsContour[0] + tagsContour[2]) * 0.5;
+
+    cv::circle(frame, tagsCenter, 16, cv::Scalar(255, 255, 0), 8);
+
+    for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+        panel[tagIndex] = getFurthestPoint(*tags[tagIndex]->getPoints(), tagsCenter);
+    }
+
+    panel[3].x = panel[1].x - (panel[1].x - (std::min(panel[0].x, panel[2].x) +
+                                             (abs(panel[0].x - panel[2].x) / 2))) * 2;
+    panel[3].y = panel[1].y - (panel[1].y - (std::min(panel[0].y, panel[2].y) +
+                                             (abs(panel[0].y - panel[2].y) / 2))) * 2;
+
+    for (int panelPointIndex = 0; panelPointIndex < panel.size(); panelPointIndex++) {
+        cv::line(frame, panel[panelPointIndex], panel[(panelPointIndex + 1) % panel.size()],
+                 cv::Scalar(0, 255, 255), 8);
     }
 }
 
 inline void FrameProcessor::getContours(cv::Mat &gray,
-                                        std::vector<std::vector<cv::Point> > &contours,
+                                        std::vector<std::vector<cv::Point2i> > &contours,
                                         std::vector<cv::Vec4i> &hierarchy) {
     cv::threshold(gray, gray, THRESHOLD, THRESHOLD_MAX, cv::THRESH_BINARY);
     findContours(gray, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
 }
 
 inline cv::Point2i
-FrameProcessor::getContourCentroid(const std::vector<cv::Point> &contour) {
+FrameProcessor::getContourCentroid(const std::vector<cv::Point2i> &contour) {
     cv::Moments moments = cv::moments(contour);
 
     return cv::Point2i((int) round(moments.m10 / moments.m00),
@@ -98,6 +142,56 @@ cv::Vec4b FrameProcessor::getMaximizedColor(const cv::Vec4b &color) {
     return maximizedColor;
 }
 
+cv::Point2i FrameProcessor::getFurthestPoint(const std::vector<cv::Point2i> &points,
+                                             const cv::Point2i &point) {
+    const cv::Point2i *closestPoint = &points[0];
+    double closestPointDistance = cv::norm(points[0] - point);
+
+    for (int pointIndex = 1; pointIndex < points.size(); pointIndex++) {
+        double distance = cv::norm(points[pointIndex] - point);
+
+        if (distance > closestPointDistance) {
+            closestPoint = &points[pointIndex];
+            closestPointDistance = distance;
+        }
+    }
+
+    return *closestPoint;
+}
+
 const cv::Vec4b &FrameProcessor::Tag::getColor() const {
     return color;
+}
+
+void FrameProcessor::Tag::notifyNewFrame() {
+    if (isSet) {
+        if (age == MAX_AGE) {
+            isSet = false;
+        } else {
+            age++;
+        }
+    }
+}
+
+void FrameProcessor::Tag::draw() const {
+    if (isSet) {
+        cv::polylines(*(frameProcessor.frame), points, true, color, DRAWING_THICKNESS);
+    }
+}
+
+void FrameProcessor::Tag::setPoints(const std::vector<cv::Point2i> &points) {
+    for (int pointIndex = 0; pointIndex < NUM_TAG_POINTS; pointIndex++) {
+        this->points[pointIndex] = points[pointIndex];
+    }
+
+    isSet = true;
+    age = 0;
+}
+
+const std::vector<cv::Point2i> *FrameProcessor::Tag::getPoints() const {
+    if (isSet) {
+        return &points;
+    } else {
+        return nullptr;
+    }
 }
--- app/src/main/cpp/FrameProcessor.h
+++ app/src/main/cpp/FrameProcessor.h
@@ -13,12 +13,33 @@ public:
 private:
     class Tag {
     public:
-        Tag(const cv::Vec4b color) : color(color) {};
+        Tag(FrameProcessor &frameProcessor, const cv::Vec4b color, bool isTopTag, bool isLeftTag) :
+                frameProcessor(frameProcessor), color(color),
+                isTopTag(isTopTag), isLeftTag(isLeftTag) {};
 
         const cv::Vec4b &getColor() const;
 
+        void notifyNewFrame();
+
+        void draw() const;
+
+        void setPoints(const std::vector<cv::Point2i> &points);
+
+        const std::vector<cv::Point2i> *getPoints() const;
+
     private:
+        static const int MAX_AGE = 4;
+
+        static const int DRAWING_THICKNESS = 8;
+
+        int age = 0;
+        bool isSet = false;
+        const bool isTopTag;
+        const bool isLeftTag;
+        const FrameProcessor &frameProcessor;
         const cv::Vec4b color;
+        std::vector<cv::Point2i> points = {cv::Point2i(0, 0), cv::Point2i(0, 0),
+                                           cv::Point2i(0, 0), cv::Point2i(0, 0)};
     };
 
     static const int THRESHOLD = 40;
@@ -35,13 +56,24 @@ private:
 
     static constexpr double APPROX_CURVE_ACCURACY_FACTOR = 0.1;
 
+    static const int NUM_TAG_POINTS = 4;
+
     cv::Mat *frame;
 
-    Tag tagBlue = Tag(cv::Vec4b(255, 0, 0));
-    Tag tagGreen = Tag(cv::Vec4b(0, 255, 0));
-    Tag tagRed = Tag(cv::Vec4b(0, 0, 255));
+    std::vector<cv::Point2i> panel = {cv::Point2i(0, 0), cv::Point2i(0, 0),
+                                      cv::Point2i(0, 0), cv::Point2i(0, 0)};
+
+    bool panelIsSet = false;
+
+    Tag tagBlue = Tag(*this, cv::Vec4b(255, 0, 0), true, true);
+    Tag tagGreen = Tag(*this, cv::Vec4b(0, 255, 0), true, false);
+    Tag tagRed = Tag(*this, cv::Vec4b(0, 0, 255), false, true);
 
     const std::vector<Tag *> tags = {&tagBlue, &tagGreen, &tagRed};
+    std::vector<cv::Point2i> tagsContour = {cv::Point2i(0, 0),
+                                            cv::Point2i(0, 0),
+                                            cv::Point2i(0, 0)};
+    cv::Point2i tagsCenter;
 
     static inline void getContours(cv::Mat &gray,
                                    std::vector<std::vector<cv::Point> > &contours,
@@ -52,6 +84,9 @@ private:
     inline Tag *getTagByColor(const cv::Vec4b &color) const;
 
     static inline cv::Vec4b getMaximizedColor(const cv::Vec4b &color);
+
+    static inline cv::Point2i getFurthestPoint(const std::vector<cv::Point2i> &points,
+                                               const cv::Point2i &point);
 };
 
 
